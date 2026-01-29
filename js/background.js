a2407a204aa1cefc56484ac6d9dfe19dab6e85c0
DZ0Phong <hiep20004@gmail.com>
2026-01-29 16:10:37 +0700

    fix

--- js/background.js
+++ js/background.js
@@ -29,7 +29,7 @@ async function setupOffscreenDocument(path) {
 }
 
 chrome.runtime.onInstalled.addListener(() => {
-    setupOffscreenDocument('offscreen.html');
+    setupOffscreenDocument('html/offscreen.html');
 });
 
 // Message Listener
